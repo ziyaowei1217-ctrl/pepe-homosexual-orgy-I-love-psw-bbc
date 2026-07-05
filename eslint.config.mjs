bebbc379d62cce29f46e76fef74e74020ca9b7e1
zihaowang20061202-max <zihaowang20061202@gmail.com>
2026-07-05 22:07:50 +0800

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: [".next/**", "node_modules/**", "api/**"]
+    ignores: [".next/**", ".next-dev/**", "node_modules/**", "api/**"]
   },
   ...compat.extends("next/core-web-vitals")
 ];
